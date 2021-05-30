4fb9703580b33ee220a314af0524caee80803aa6
Koren <korensb@gmail.com>
2021-05-30 19:31:02 +0300

    remove Gilad

--- gilad/test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ChessSystem/gilad/ex1-public
-# Build directory: D:/ChessSystem/gilad/ex1-public/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/ChessSystem/gilad/ex1-public/build/own_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
