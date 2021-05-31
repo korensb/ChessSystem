6cd2d15da2d9decf3fd9d36e0b2ed350f7409d19
Koren <korensb@gmail.com>
2021-05-31 17:48:41 +0300

    fixes to dry

--- dry/dry.c
+++ dry/dry.c
@@ -19,14 +19,18 @@ char* reverseEvenString (char* str, int* x)  //error 1 - we need to give the fun
     }
     for (i = 0; i < *x; i++) //error 8 - we need to make an indent line and use {}
     {
-        str2[i] = str[*x -i-1]; //error 9 - we need to add '-1' to the i index to not slip out of array
+        str2[i] = str[*x -i-1]; //error 9 - don't want to copy the null space: need to add '-1' to the index we access. 
     }
-    if (*x % 2 == 0) {
+    str2[*x] = '\0'; //error 10 - needs to add the null space to the end of the string
+
+    if (*x % 2 != 0) // error 11 - needs to print the origin string if the length is odd, changed the condition.
+    {
         printf("%s", str);
     }
-    if (*x % 2 != 0)
+    if (*x % 2 == 0) // error 12 - needs to print the reverse string if the length is even, changed the condition.
     {
         printf("%s", str2);
     }
+
     return str2;
 }
