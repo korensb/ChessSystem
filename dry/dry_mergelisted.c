d3c0ce08e7bc086dc267cf8c306d705a5be36c6d
Arad <arad.halevy@gmail.com>
2021-05-31 17:38:29 +0300

    little conventions change

--- dry/dry_mergelisted.c
+++ dry/dry_mergelisted.c
@@ -60,85 +60,81 @@ Node addNodeToList (Node current , int value, Node head, ErrorCode* error_code)
 
 Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
 {
-   {
-        if (list1 == NULL || list2 == NULL)
-        {
-            *error_code = NULL_ARGUMENT;
-            return NULL;
-        }
-        if (isListSorted(list1) != true || isListSorted(list2) != true)
-        {
-            *error_code = UNSORTED_LIST;
-            return NULL;
-        }
+    if (list1 == NULL || list2 == NULL)
+    {
+        *error_code = NULL_ARGUMENT;
+        return NULL;
+    }
+    if (isListSorted(list1) != true || isListSorted(list2) != true)
+    {
+        *error_code = UNSORTED_LIST;
+        return NULL;
+    }
 
-        Node head;
+    Node head;
 
+    if (list1->x < list2->x)
+    {
+        head = createNode(list1->x);
+        if (head == NULL)
+            {
+                *error_code = MEMORY_ERROR;
+                return NULL;
+            }
+        list1 = list1->next;
+    }
+    else
+    {
+        head = createNode(list2->x);
+        if (head == NULL)
+            {
+                *error_code = MEMORY_ERROR;
+                return NULL;
+            }
+        list2 = list2->next;
+    }
+    Node current = head;
+    while (list1 != NULL && list2 != NULL)
+    {
         if (list1->x < list2->x)
         {
-            head = createNode(list1->x);
-            if (head == NULL)
-                {
-                    *error_code = MEMORY_ERROR;
-                    return NULL;
-                }
+            current = addNodeToList(current , list1->x, head, error_code);
+            if (current == NULL)
+            {
+                return NULL;
+            }
             list1 = list1->next;
         }
         else
         {
-            head = createNode(list2->x);
-            if (head == NULL)
-                {
-                    *error_code = MEMORY_ERROR;
-                    return NULL;
-                }
-            list2 = list2->next;
-        }
-
-        Node current = head;
-        while (list1 != NULL && list2 != NULL)
-        {
-            if (list1->x < list2->x)
-            {
-                current = addNodeToList(current , list1->x, head, error_code);
-                if (current == NULL)
-                {
-                    return NULL;
-                }
-                list1 = list1->next;
-            }
-            else
-            {
-                current = addNodeToList(current , list2->x, head, error_code);
-                if (current == NULL)
-                {
-                    return NULL;
-                }
-                list2 = list2->next;
-            }
-        }
-
-        while (list1 != NULL)
-        {
-            current = addNodeToList(current , list1->x, head, error_code);
-            if (current == NULL)
-                {
-                    return NULL;
-                }
-            list1 = list1->next;
-        }
-
-        while (list2 != NULL)
-        {
             current = addNodeToList(current , list2->x, head, error_code);
             if (current == NULL)
-                {
-                    return NULL;
-                }
+            {
+                return NULL;
+            }
             list2 = list2->next;
         }
-        
-        *error_code = SUCCESS;
-        return head;
     }
+
+    while (list1 != NULL)
+    {
+        current = addNodeToList(current , list1->x, head, error_code);
+        if (current == NULL)
+            {
+                return NULL;
+            }
+        list1 = list1->next;
+    }
+
+    while (list2 != NULL)
+    {
+        current = addNodeToList(current , list2->x, head, error_code);
+        if (current == NULL)
+            {
+                return NULL;
+            }
+        list2 = list2->next;
+    }
+    *error_code = SUCCESS;
+    return head;
 }
