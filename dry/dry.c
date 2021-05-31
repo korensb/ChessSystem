ff8bd9b6f2938c0a4052307e23a0fa186aeedf64
Arad <arad.halevy@gmail.com>
2021-05-31 16:27:28 +0300

    dry update

--- dry/dry.c
+++ dry/dry.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char* foo(char* str, int* x) {
-	if (str == NULL)
-	{
-		return 0;
-	}
-	if (x == NULL)
-	{
-		return 0;
-	}
-	char* str2;
-	int i;
-	x = strlen(str);
-	str2 = malloc(sizeof(int)*(*x));
-	if (str2 == NULL)
-	{
-		return 0;
-	}
-	for (i = 0; i < *x; i++)
-		str2[i] = str[*x - i - 1];
-	if (*x % 2 == 0) {
-		printf("%s", str);
-	}
-	if (*x % 2 != 0)
-	{
-		printf("%s", str2);
-	}
-	return str2;
+char* reverseEvenString (char* str, int* x)  //error 1 - we need to give the function a meaningfull name
+    {  //error 2 - we need to give the { of a function its own line
+    if (str == NULL){  //error 3 - we need to check if str is a valid pointer
+        return NULL;
+    }NULL
+    if (x == NULL){  //error 4 - we need to check check if x is a valid pointer
+        return NULL;
+    }
+    char* str2;
+    int i;
+    *x = strlen(str); // error 5 - we need to derefrence the pointer to put the value in it
+    str2 = malloc(sizeof(int)*(*x)); //error 6 - we need to multiply the memory needed with sizeof int
+    if (str2 == NULL){
+        return NULL; //error 7 - we need to make sure malloc succeded
+    }
+    for (i = 0; i < *x; i++) //error 8 - we need to make an indent line and use {}
+    {
+        str2[i] = str[*x -i-1]; //error 9 - we need to add '-1' to the i index to not slip out of memory
+    }
+    if (*x % 2 == 0) {
+        printf("%s", str);
+    }
+    if (*x % 2 != 0)
+    {
+        printf("%s", str2);
+    }
+    free (str2); //error 10 - we need to free the memory allocated
+    return str2;
 }
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 CHESS = chess
 GCC = gcc
-OBJS = chess.o game.o tournament.o player.o chessSystemTestsExamplemaarouf.o
+OBJS = chess.o game.o tournament.o player.o chessSystemTestsExample.o
 COMP_FLAG = -std=c99 -Wall -pedantic -errors -Werror 
 DEBUG_FLAG =  -g
  
 $(CHESS) :$(OBJS)
 	$(GCC) $(DEBUG_FLAG) $(OBJS)  -o $@ -L. -lmap
-chessSystemTestsExamplemaarouf.o : tests/chessSystemTestsExamplemaarouf.c chessSystem.h map.h test_utilities.h
+chessSystemTestsExample.o : tests/chessSystemTestsExample.c chessSystem.h map.h test_utilities.h
 	$(GCC) -c $(DEBUG_FLAG) $(COMP_FLAG) $<
 chess.o : chess.c chessSystem.h game.h tournament.h player.h map.h
 	$(GCC) -c $(DEBUG_FLAG) $(COMP_FLAG) $*.c
